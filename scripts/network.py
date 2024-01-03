8ce93c3f3dcc18abe57c7448d9567ccc78d34e13
tbaradatfour <teo.baradat-fourane@etu.u-bordeaux.fr>
2024-01-03 17:30:02 +0100

    new strips

--- scripts/network.py
+++ scripts/network.py
@@ -34,7 +34,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 transform = transforms.Compose(
     [transforms.Normalize((0.5,), (0.5,))])
 
-c = ds.CustomImageDataset("./data/shite.csv", "./data/train_pics", transform=transform)
+c = ds.CustomImageDataset("./data/train.csv", "./data/train_pics", transform=transform)
 bs=8
 trainloader = torch.utils.data.DataLoader(c, batch_size=bs, shuffle=True, num_workers=2)
 
@@ -43,22 +43,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # functions to show an image
-
-
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
-
 classes = [chr(ord("a")+i) for i in range(26)]
 
-for epoch in range(20):  # loop over the dataset multiple times
+for epoch in range(10):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -92,7 +85,7 @@ net.load_state_dict(torch.load(PATH))'''
 
 transform = transforms.Compose([transforms.PILToTensor()])
 
-im = Image.open("./data/to_predict.png")
+im = Image.open("./to_predict.png")
 image_tensor = transform(im)
 
 im_ten = image_tensor.type(torch.float)
