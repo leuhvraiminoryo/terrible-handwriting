c7c7aa50bbcaaa06d061b1c01844621639f9ed53
tbaradatfour <teo.baradat-fourane@etu.u-bordeaux.fr>
2024-01-05 15:16:05 +0100

    'final' tests with newly added strips - satisfactory state

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ traite les images en 'alphabet_strips/' pour remplir/remplacer les éléments du
 '''
 #dec.treat_data("alphabet_strips/")
 
-nn = net.get_network()
+nn = net.get_network(retrain=False) # loads aux/letters_net.pth | trains the network from scrtach if passed 'True'
 net.test_single_image(nn)
-net.test(nn)
+net.test(nn) # tests how 'good' the network is, printing out global accuracy and class-wise accuracy
